73b2ce833f2e7a894a8c7840fcdeae84bae60e9d
zbrucyp <zbrucyp@gmail.com>
2020-02-26 13:47:42 -0500

    arrange noteFragment layout, add fragment manager and fragment in main activity

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.todolist;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
@@ -10,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // fragment setup
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+
+        if (fragment == null) {
+            fragment = new NoteFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragmentContainer, fragment)
+                    .commit();
+        }
     }
 }
---
+++ app/src/main/java/com/example/todolist/NoteFragment.java
@@ -0,0 +1,69 @@
+package com.example.todolist;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import androidx.fragment.app.Fragment;
+
+
+public class NoteFragment extends Fragment {
+
+    private Note mNote;
+    private EditText mTitleField;
+    private Button mDateButton;
+    private CheckBox mDoneCheckBox;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mNote = new Note();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.fragment_note, container, false);
+
+        mTitleField = (EditText) v.findViewById(R.id.note_title);
+        mTitleField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mNote.setTitle((s.toString()));
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        mDateButton = (Button) v.findViewById((R.id.note_date));
+        mDateButton.setText(mNote.getDate().toString());
+        mDateButton.setEnabled(false);
+
+        mDoneCheckBox = (CheckBox) v.findViewById(R.id.job_done);
+        mDoneCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                mNote.setDone(isChecked);
+            }
+        });
+
+        return v;
+    }
+
+}
