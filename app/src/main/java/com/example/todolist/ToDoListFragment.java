a2d8c3e44b4908204c9a63bcf67cac08d4c3ab42
zbrucyp <zbrucyp@gmail.com>
2020-03-02 11:40:14 -0500

    add framework

---
+++ app/src/main/java/com/example/todolist/ToDoListFragment.java
@@ -0,0 +1,84 @@
+package com.example.todolist;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class ToDoListFragment extends Fragment {
+
+    private RecyclerView mRecyclerView;
+    private NoteAdapter mAdapter;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_to_do_list, container, false);
+
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.note_recycler_view);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        updateUI();
+
+        return view;
+    }
+
+    public void updateUI() {
+        NoteBook noteBook = NoteBook.get(getActivity());
+        List<Note> notes = noteBook.getNotes();
+
+        mAdapter = new NoteAdapter(notes);
+        mRecyclerView.setAdapter(mAdapter);
+    }
+
+
+    private class NoteHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        public NoteHolder(LayoutInflater inflater, ViewGroup parent) {
+            super(inflater.inflate(R.layout.list_item_note, parent, false));
+
+            itemView.setOnClickListener(this);
+        }
+
+        public void bind(Note note) {
+            //TODO: probably needs filled out
+        }
+
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(getActivity(), mNote.getTitle() + " clicked!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private class NoteAdapter extends RecyclerView.Adapter<NoteHolder> {
+        private List<Note> mNotes;
+
+        public NoteAdapter(List<Note> notes) {
+            mNotes = notes;
+        }
+
+        @Override
+        public NoteHolder onCreateViewHolder (ViewGroup parent, int viewType) {
+            LayoutInflater layoutinflater = LayoutInflater.from(getActivity());
+
+            return new NoteHolder(layoutinflater, parent); // idk if this is right
+        }
+
+        @Override
+        public void onBindViewHolder (NoteHolder holder, int position) {
+            Note note = mNotes.get(position);
+            holder.bind(note);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mNotes.size();
+        }
+    }
+}
+
